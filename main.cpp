575767c72e7f9a2ca1afe635379c853f5925ef28
Christina Song <christina.song202@gmail.com>
2021-12-28 19:52:03 -0500

    deal cards

--- main.cpp
+++ main.cpp
@@ -14,33 +14,31 @@ const int NUM_VALID_GAMES = 1;
 const int MAX_GAME_NAME = 100;
 string validGameChoices[][MAX_GAME_NAME] = {{"1", "Go Fish"}};
 
-// playing games
-string selectGame() {
-    string input;
-    cout << "\n" << consoleLine << endl;
-    cout << "Please select a game to play: " << endl;
-    for (int i = 1; i < NUM_VALID_GAMES + 1; i++) {
-        cout << i << ". " << validGameChoices[i-1][1] << endl;
-    }
-    cin >> input;
-    cout << consoleLine << "\n" << endl;
-    return input;
-}
-void showGameChoice(string userChoice) {
-    cout << "You chose to play " << validGameChoices[stoi(userChoice)-1][1] << "!\n";
-    cout << "\n" << consoleSep << endl;
-}
-void playGoFish() {
+// ---- FUNCTIONS ----
 
+// testing utils
+void printStrArray(string arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "\"" << arr[i] << "\" ";
+//        cout << arr[i] << " ";
+    }
+    printf("\n");
+}
+void printArray(char** ptr, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << *(ptr+i) << " ";
+    }
+    printf("\n");
 }
 // game functionalities
-void shuffleDeck(string deck[], int n) {
-    for (int i = 0; i < n; i++) {
-        int random = rand() % n;
-        string temp = deck[random];
-        deck[random] = deck[i];
-        deck[i] = temp;
-    }
+void fillCard(char* card, char val1, char val2, char suit) {
+    card[0] = val1, card[1] = val2, card[2] = suit;
+//    cout << card[0] << card[1] << card[2] << " ";
+}
+char* copyCard(char* card) {
+    char* copyCard = (char*)malloc(3 * sizeof(char));
+    copyCard[0] = card[0], copyCard[1] = card[1], copyCard[2] = card[2];
+    return copyCard;
 }
 void shuffleDeck(char** deck, int n) {
     for (int i = 0; i < n; i++) {
@@ -58,11 +56,20 @@ bool checkValidGameChoice(string userChoice) {
     }
     return false;
 }
-// functional utils
-void fillCard(char* card, char val1, char val2, char suit) {
-    card[0] = val1, card[1] = val2, card[2] = suit;
-//    cout << card[0] << card[1] << card[2] << " ";
+char** copyDeck(char** deck, int n) {
+    char **copyDeck = (char**)malloc(n * sizeof(char*));
+    if (copyDeck == NULL) {
+        cout << "no more space" << endl;
+        return NULL;
+    }
+    for (int i = 0; i < n; i++) {
+        copyDeck[i] = (char*)malloc(3 * sizeof(char));
+        fillCard(copyDeck[i], deck[i][0], deck[i][1], deck[i][2]);
+    }
+    printArray(copyDeck, n);
+    return copyDeck;
 }
+// functional utils
 bool strArrayContains(string arr[], int n, string elem) {
     for (int i = 0; i < n; i++) {
         if (arr[i] == elem) {
@@ -71,20 +78,63 @@ bool strArrayContains(string arr[], int n, string elem) {
     }
     return false;
 }
-// testing utils
-void printStrArray(string arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << "\"" << arr[i] << "\" ";
-//        cout << arr[i] << " ";
+
+// PLAYING GAMES
+// game selection
+string selectGame() {
+    string input;
+    cout << "\n" << consoleLine << endl;
+    cout << "Please select a game to play: " << endl;
+    for (int i = 1; i < NUM_VALID_GAMES + 1; i++) {
+        cout << i << ". " << validGameChoices[i-1][1] << endl;
     }
-    printf("\n");
+    cin >> input;
+    cout << consoleLine << "\n" << endl;
+    return input;
 }
-void printArray(char** ptr, int n) {
-    for (int i = 0; i < n; i++) {
-        cout << *(ptr+i) << " ";
+void showGameChoice(string userChoice) {
+    cout << "You chose to play " << validGameChoices[stoi(userChoice)-1][1] << "!\n";
+    cout << "\n" << consoleSep << endl;
+}
+// common game utils
+char*** dealCards(char** deck, int deckSize, int numToDeal) {
+    char*** dealtCards = (char***)malloc(2 * sizeof(char**));
+    for (int i = 0; i < 2; i++) {
+        dealtCards[i] = (char**)malloc(3 * numToDeal * sizeof(char*));
+        for (int j = 0; j < numToDeal; j++) {
+            dealtCards[i][j] = (char*)malloc(3 * sizeof(char));
+        }
     }
-    printf("\n");
+
+    int p1 = 0, p2 = 0;
+    for (int i = 0; i < 2*numToDeal; i++) {
+        if (i%2 == 0) {
+            dealtCards[0][p1] = copyCard(deck[i]);
+            p1++;
+        }
+        else {
+            dealtCards[1][p2] = copyCard(deck[i]);
+            p2++;
+        }
+    }
+
+    return dealtCards;
 }
+
+// GO FISH
+void playGoFish(char** origCardDeck, int origDeckSize) {
+    char** stock = copyDeck(origCardDeck, origDeckSize);
+
+    // deal cards
+    char*** dealtCards = dealCards(stock, origDeckSize, 7);
+    char** userCards = dealtCards[0];
+    char** compCards = dealtCards[1];
+    cout << "user's cards: " << endl;
+    printArray(userCards, 7);
+    cout << "computer's cards: " << endl;
+    printArray(compCards, 7);
+}
+
 
 int main()
 {
@@ -144,6 +194,17 @@ int main()
         userChoice = selectGame();
     }
     showGameChoice(userChoice);
+
+    // play game
+    if (userChoice == "1") {
+        playGoFish(cardDeck, DECK_SIZE);
+    }
+    else {
+        cout << "Something went wrong :(" << endl;
+        return -1;
+    }
+
+    // ---- END STUFF ----
 
     // deallocating memory
     free(cardSuits);
